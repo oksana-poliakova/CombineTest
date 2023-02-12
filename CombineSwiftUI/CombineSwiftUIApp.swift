9e6c38e3684e46b527ef3dfd88273a0563edb5f9
Oksana Poliakova <it.oksana.poliakova@gmail.com>
2023-02-12 17:25:50 +0200

    Initial Commit

---
+++ CombineSwiftUI/CombineSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  CombineSwiftUIApp.swift
+//  CombineSwiftUI
+//
+//  Created by Oksana Poliakova on 12.02.2023.
+//
+
+import SwiftUI
+
+@main
+struct CombineSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
